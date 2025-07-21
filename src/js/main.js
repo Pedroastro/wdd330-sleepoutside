3930e75c2f2ca4281ff4bc95c801886168a5c9ca
pedroastro <pedromscastro9095@gmail.com>
2025-07-21 19:25:59 -0300

    add W02 Individual Activity format and lint

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 
-const product = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const productList = new ProductList("tents", product, element);
+const product = new ProductData('tents');
+const element = document.querySelector('.product-list');
+const productList = new ProductList('tents', product, element);
 productList.init();
